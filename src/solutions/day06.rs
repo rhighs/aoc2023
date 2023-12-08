152d863838067f140e15e3872b0e19d9ceb2e1b7
roberto montalti <roberto.montalti00@gmail.com>
2023-12-08 18:05:10 +0100

    Day 6

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,7 +1,61 @@
-pub fn p1(_input: &String) -> String {
-    todo!()
+use itertools::Itertools;
+
+fn parse_input_1(input: &String) -> Vec<(usize, usize)> {
+    let (t, d) = input
+        .lines()
+        .map(|v| {
+            v.chars()
+                .skip_while(|&c| c != ':')
+                .skip(1)
+                .collect::<String>()
+        })
+        .map(|s| {
+            s.split_whitespace()
+                .map(|v| v.parse::<usize>().unwrap())
+                .collect_vec()
+        })
+        .collect_tuple()
+        .unwrap();
+    t.into_iter().zip(d).collect_vec()
 }
 
-pub fn p2(_input: &String) -> String {
-    todo!()
+fn parse_input_2(input: &String) -> (usize, usize) {
+    let (t, d) = input
+        .lines()
+        .map(|v| {
+            v.chars()
+                .skip_while(|&c| c != ':')
+                .skip(1)
+                .collect::<String>()
+        })
+        .map(|s| {
+            s.chars()
+                .filter(|&c| c != ' ')
+                .collect::<String>()
+                .parse::<usize>()
+        })
+        .collect_tuple()
+        .unwrap();
+    t.into_iter().zip(d).collect_vec().pop().unwrap()
+}
+
+fn ways((t, d): (usize, usize)) -> usize {
+    (1..=(t / 2)).fold(0, |acc, v| {
+        if v * (t - v) > d {
+            acc + if v == (t - v) { 1 } else { 2 }
+        } else {
+            0
+        }
+    })
+}
+
+pub fn p1(input: &String) -> String {
+    parse_input_1(input)
+        .iter()
+        .fold(1, |acc, &v| acc * ways(v))
+        .to_string()
+}
+
+pub fn p2(input: &String) -> String {
+    ways(parse_input_2(input)).to_string()
 }
